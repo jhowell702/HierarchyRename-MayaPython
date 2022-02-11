83d6b49ed9241c49bf1ba1fab1c4c4097cca10c6
Justin Howell <48781401+jhowell702@users.noreply.github.com>
2022-02-11 04:53:32 -0800

    Add files via upload

---
+++ hierachRename.py
@@ -0,0 +1,60 @@
+# Import the Maya commands library
+from maya import cmds
+
+findString = 'null'
+replaceString = 'null'
+
+def showWindow():
+    window = cmds.window( title="Auto Merge Tool", iconName='AMT', widthHeight=(275, 150) )
+
+    # As we add contents to the window, align them vertically
+    cmds.columnLayout( adjustableColumn=True )
+
+    cmds.rowLayout(numberOfColumns=2)
+
+    cmds.text( 'Find String: ' )
+    findField = cmds.textField()
+    cmds.textField( findField, edit=True, aie=True, changeCommand=lambda x: setFindString(findField) )
+
+    cmds.setParent('..')
+
+
+    cmds.rowLayout(numberOfColumns=2)
+
+    cmds.text( 'Replace String: ' )
+    replaceField = cmds.textField()
+
+    cmds.textField( replaceField,edit=True, aie=True, changeCommand=lambda x: setReplaceString(replaceField) )
+
+    cmds.setParent('..')
+
+    cmds.button( label='Search and Replace', command=replaceHiearchy )
+
+    cmds.button( label='Close', command=('cmds.deleteUI(\"' + window + '\", window=True)') )
+
+    cmds.setParent( '..' )
+
+ 
+
+    # Show the window that we created (window)
+    cmds.showWindow( window )
+
+def setFindString(*args):
+    global findString
+    findString = cmds.textField(args[0], q=1, text=1)
+
+def setReplaceString(*args):
+    global replaceString
+    print(args[0])
+    replaceString = cmds.textField(args[0], q=1, text=1)
+
+def replaceHiearchy(*args):
+    children_nodes = cmds.listRelatives(allDescendents=True)
+    print(findString)
+    print(replaceString)
+    cmds.select(children_nodes, add=True)
+    nodes = cmds.ls(sl=True, shapes=False)
+    for obj in nodes:
+        temp = obj
+        temp = temp.replace(findString, replaceString)
+        cmds.rename(obj, temp)
